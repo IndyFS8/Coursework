cbeefb1d779574b471f62e238e2edf17abce1b40
Indy <70581779+IndyFS8@users.noreply.github.com>
2022-11-09 11:52:21 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config('SQLALCHEMY_DATABASE_URI') = 'sqlite:///database.db'
+app.config('SECRET_KEY') = 'thisisasecretkey'
 
 
+class User(db.Model, UserMixi):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.string(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
