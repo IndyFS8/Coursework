c374c567a10b60c680813e4e0da178eea832bf6a
Indy <70581779+IndyFS8@users.noreply.github.com>
2022-11-16 11:46:19 +0000

    started database stuffs

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
+import WTF as WTF
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from flask-WTF import wtforms
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SECRET_KEY'] = "thisisasecretkey"
 db = SQLAlchemy(app)
-app.config('SQLALCHEMY_DATABASE_URI') = 'sqlite:///database.db'
-app.config('SECRET_KEY') = 'thisisasecretkey'
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.string(20), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -28,3 +35,6 @@ def register():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
